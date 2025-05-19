4c8837580f5821ad2887c362d0fe84a856be25e9
VictorGuerra <Victorguerra2310@gmail.com>
2025-05-18 21:06:24 -0300

    new

---
+++ Projeto Extensao/dados.js
@@ -0,0 +1,17 @@
+const esportistas = [
+  {
+    nome: "Lucas Oliveira",
+    modalidade: "Futebol",
+    descricao: "Zagueiro do time X, campeão municipal em 2023.",
+    foto: "https://link-da-foto.jpg",
+    instagram: "https://instagram.com/lucas_atleta",
+    whatsapp: "https://wa.me/55XXXXXXXXXXX"
+  },
+  {
+    nome: "Fernanda Silva",
+    modalidade: "Judô",
+    descricao: "Faixa marrom, bicampeã estadual.",
+    foto: "https://link-da-foto.jpg",
+    instagram: "https://instagram.com/fernandajudo"
+  }
+];
